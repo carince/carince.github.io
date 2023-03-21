fc95553c50717e3cd99a74fe3903d9ab0725fa26
Norikiru <99774021+Norikiru@users.noreply.github.com>
2023-03-22 00:06:10 +0800

    main: add blur to cards public: clean up index.html

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -10,11 +10,11 @@ export default function Main() {
                     <p>professional web/app developer</p>
                 </div>
             </div>
-            <div className='About border rounded-3 shadow-lg p-3 my-4'>
+            <div className='Card About border rounded-3 shadow-lg p-3 my-4'>
                 <p className='Title fw-bold fs-3'> About Me </p>
                 <p className='Description fw-light fs-5 m-0'> A (soon2be) software engineer with my area of expertise in creating applications with occasional involvement in designing.  </p>
             </div>
-            <div className='Stack border rounded-3 shadow-lg p-3 my-4'>
+            <div className='Card Stack border rounded-3 shadow-lg p-3 my-4'>
                 <p className='Title fw-bold fs-3'> Tech Stack </p>
                 <p className='Description fw-light fs-5 m-0'> JavaScript, TypeScript, Node.js, Electron, React, MIT </p>
             </div>
