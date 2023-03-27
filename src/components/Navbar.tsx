9b89d0f5a28660cf0ebbfd6ddb05859aadccbd61
Norikiru <99774021+Norikiru@users.noreply.github.com>
2023-03-27 19:39:24 +0800

    navbar: replace navigation with contacts navbar: add onhover effects home/intro: center and use full screen home/tech: add tech stack

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,23 @@
 import { ReactComponent as Logo } from '../assets/Logo.svg'
-
-const navigation = [
-    {
-        text: "Home",
-        path: "/"
-    },
-    {
-        text: "Contact",
-        path: "/contact"
-    }
-]
+import { ReactComponent as GitHub } from '../assets/github.svg'
+import { ReactComponent as Email } from '../assets/email.svg'
 
 export default function Navbar() {
     return (
-        <div className="Header top-0 w-full flex-none py-4 px-14 border-b border-zinc-700 bg-zinc-800 text-white">
+        <nav className="Header fixed top-0 w-full flex-none py-4 px-14 border-b border-zinc-700 backdrop-blur bg-zinc-900/95  text-white">
             <div className="Header-Inner flex items-center">
-                <Logo className="Header-Log fill-white"></Logo>
+                <Logo className="Header-Log fill-white hover:fill-zinc-300"></Logo>
                 <nav className='Header-Nav relative ml-auto items-center'>
-                    <ul className='flex space-x-8'>
-                        {navigation.map(navigate =>
-                            <li><a href={navigate.path} className="font-semibold">{navigate.text}</a></li>
-                        )}
-                    </ul>
+                    <div className='flex space-x-8'>
+                        <a href="https://github.com/carince" className="font-semibold ">
+                            <GitHub className="fill-white hover:fill-zinc-300 h-[36px] w-max" />
+                        </a>
+                        <a href="mailto:carince.official@gmail.com" className="font-semibold ">
+                            <Email className="fill-white hover:fill-zinc-300 h-[36px] w-max" />
+                        </a>
+                    </div>
                 </nav>
             </div>
-        </div>
+        </nav>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,50 @@
 import { ReactComponent as Logo } from '../assets/Logo.svg'
 import Background from '../assets/bg.png'
+import Topograph from '../assets/topograph.png'
 
 function Home() {
-  return (
-    <div className='Home w-full flex flex-col justify-center align-middle p-10 space-y-16 bg-[#121212] text-white'>
-      <div className="Home-Intro sm:flex items-center sm:space-x-8">
-        <Logo className='Intro-Logo fill-white h-64 w-64' />
-        <div className='Intro-Text font-light text-4xl sm:text-5xl'>
-          /un/professional <br />web and app <br />developer
+    return (
+        <div className='Home flex flex-col bg-[#161616] text-white'>
+
+            <div className="Home-Intro h-screen flex flex-col sm:flex-row justify-center items-center sm:space-x-8 bg-fixed bg-no-repeat" style={{
+                backgroundImage: `url(${Background})`,
+            }}>
+                <Logo className='Intro-Logo fill-white h-64 w-64 bg-fixed bg-no-repeat' />
+                <div className='Intro-Text font-light text-4xl sm:text-5xl'>
+                    /un/professional <br />web and app <br />developer
+                </div>
+            </div>
+
+            <div className="Home-AboutMe sm:flex flex-col space-y-3 p-14">
+                <p className='AboutMe-Header font-extrabold text-4xl sm:text-5xl'>
+                    About Me
+                </p>
+                <p className='AboutMe-Text font-light text-xl sm:text-3xl'>
+                    Hi there! My name is carince, and I am a 15-year-old from the Philippines with a passion for web and app development.
+                    I was born on April 9th, 2007, and I have been creating apps and websites since I was just 12 years old.
+                    I love the process of conceptualizing an idea, writing the code, and seeing it come to life.
+                    Every project is a new challenge that requires me to think critically and creatively,
+                    and I enjoy the satisfaction of finding solutions to complex problems.
+                    I believe that software has the power to make a positive impact on the world, and I am excited to continue
+                    honing my skills and contributing to its development. In my spare time, I also contribute to open-source projects on GitHub,
+                    as I believe in the power of Open Source and the impact it can have on the Tech Space.
+                </p>
+            </div>
+
+            <div className="Home-TechStack sm:flex flex-col space-y-3 p-14 bg-fixed bg-no-repeat" style={{
+                backgroundImage: `url(${Topograph})`,
+            }}>
+                <p className='TechStack-Header font-extrabold text-4xl sm:text-5xl'>
+                    Tech Stack
+                </p>
+                <p className='TechStack-Text font-light text-xl sm:text-3xl'>
+                    Stuff I use in developing!
+
+                    JavaScript, TypeScript, Node.js, React, Tailwind, HTML/CSS
+                </p>
+            </div>
         </div>
-      </div>
-
-      <div className="Home-AboutMe sm:flex flex-col space-y-3">
-        <p className='AboutMe-Header font-extrabold text-4xl sm:text-5xl'>
-          About Me
-        </p>
-        <p className='AboutMe-Text font-light text-xl sm:text-3xl'>
-        Hi there! I am carince, and I am a 15-year-old from the Philippines with a passion for web and app development. 
-        I was born on April 9th, 2007, and I have been creating apps and websites since I was just 12 years old. 
-        I love the process of conceptualizing an idea, writing the code, and seeing it come to life. 
-        Every project is a new challenge that requires me to think critically and creatively, 
-        and I enjoy the satisfaction of finding solutions to complex problems. 
-        I believe that software has the power to make a positive impact on the world, and I am excited to continue 
-        honing my skills and contributing to its development. In my spare time, I also contribute to open-source projects on GitHub, 
-        as I believe in the power of Open Source and the impact it can have on the Tech Space.
-        </p>
-      </div>
-
-      <div className="Home-Skills sm:flex flex-col space-y-3">
-        <p className='Skills-Header font-extrabold text-4xl sm:text-5xl'>
-          Things I use
-        </p>
-        <p className='AboutMe-Subheader font-extralight text-md sm:text-xl'>
-          Here are the software that I use in developing!
-        </p>
-        <p className='AboutMe-Text font-light text-xl sm:text-3xl'>
-          TODO
-        </p>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Home
